a0fe6dc14cd44ff7f31f4b8728409b029c72df65
Garrett Kucinski <garrettryan@doejo.com>
2017-04-20 17:38:04 -0500

    finalized search pagination

--- js/app.js
+++ js/app.js
@@ -6,13 +6,21 @@
 "use strict"; // no monkey business
 
 const students = document.querySelectorAll('.student-item'),
+    page = document.getElementsByClassName('page')[0],
+    // Create container for the pagination element
+    pagination = document.createElement('UL'),
 
     // Changing this number changes pages generated as well as
     // configuring how many students to show and updates the pagination
     // click event accordingly
     studentsToShow = 10;
 
-let startingStudentIndex = 0;
+let startingStudentIndex = 0,
+    numStudents = document.getElementsByClassName('student-item').length;
+
+pagination.id = 'pagination-list';
+pagination.className = 'pagination-list';
+page.appendChild(pagination);
 
 function createSearchBox() {
     const searchBoxMarkup = `<input placeholder="Search for students...">
@@ -27,18 +35,24 @@ function createSearchBox() {
     pageHeader.appendChild(studentSearch);
 }
 
-function generatePagination() {
+function displayStudents(students) {
+    for (let i = 0; i < students.length; i++) {
+        students[i].style.display = 'none';
+    }
+
+    for (let i = startingStudentIndex; i < (startingStudentIndex + studentsToShow); i++) {
+        try {
+            students[i].style.display = 'block';
+        } catch (e) {
+            break;
+        }
+    }
+}
+
+function generatePagination(students) {
     // get the page div, total number of students and calculate
     // the number of pages that need to be generated
-    const page = document.getElementsByClassName('page')[0],
-        numStudents = document.getElementsByClassName('student-item').length,
-        numPages = Math.ceil(numStudents / studentsToShow);
-
-    // Create container for the pagination element
-    const pagination = document.createElement('UL');
-    pagination.id = 'pagination-list';
-    pagination.className = 'pagination-list';
-
+    const numPages = Math.ceil(numStudents / studentsToShow);
     let pageList = '';
 
     // Loop through the number of pages and assign a number
@@ -55,35 +69,13 @@ function generatePagination() {
     // to the list items generated above
     pagination.innerHTML = pageList;
 
-    // Append the entire pagination element to the page element 
-    page.appendChild(pagination);
-}
-
-function displayStudents() {
-    for (let i = 0; i < students.length; i++) {
-        students[i].style.display = 'none';
-    }
-
-    for (let i = startingStudentIndex; i < (startingStudentIndex + studentsToShow); i++) {
-        try {
-            students[i].style.display = 'block';
-        } catch (e) {
-            break;
-        }
-    }
-}
-
-function filterStudents() {
-    // When a use clicks on a page number, they should see ten results
-    displayStudents();
-
     const pageMarker = document.getElementsByClassName('page-marker');
 
     // Loop through students and attach event listeners 
     for (let i = 0; i < pageMarker.length; i++) {
         pageMarker[i].addEventListener('click', () => {
             startingStudentIndex = (pageMarker[i].value * studentsToShow) - studentsToShow;
-            displayStudents();
+            displayStudents(students);
             let activeLinks = document.querySelectorAll('.page-marker.active');
             for (let i = 0; i < activeLinks.length; i++) {
                 activeLinks[i].classList.remove('active');
@@ -94,48 +86,58 @@ function filterStudents() {
 }
 
 function search() {
-    document.getElementById('pagination-list').style.display = 'none';
 
     for (let i = 0; i < students.length; i++) {
         students[i].style.display = 'none';
     }
 
-    let searchTerm = document.getElementById('student-search').firstElementChild.value;
-    const foundStudents = [];
+    let searchTerm = document.getElementById('student-search').firstElementChild.value.trim();
+
+    const searchResults = [];
 
     for (let i = 0; i < students.length; i++) {
-        const studentName = students[i].children[0].childNodes[3].textContent;
-        const studentEmail = students[i].children[0].childNodes[5].textContent;
+        const studentName = students[i].children[0].childNodes[3].textContent.trim();
+        const studentEmail = students[i].children[0].childNodes[5].textContent.trim();
         if (studentName.indexOf(searchTerm) !== -1 || studentEmail.indexOf(searchTerm) !== -1) {
-            students[i].style.display = 'block';
-            foundStudents.push(students[i]);
+            searchResults.push(students[i]);
+            console.log(students[i].innerText);
         }
     }
-    if (!foundStudents.length) {
+    console.log(searchResults);
+    if (!searchResults.length) {
         const searchError = document.createElement('P');
         searchError.innerHTML = 'Sorry, no students found';
         searchError.className = 'search-error';
         searchError.id = 'search-error';
-
-        document.getElementsByClassName('page')[0].appendChild(searchError);
+        page.appendChild(searchError);
+    } else {
+        numStudents = searchResults.length;
+        generatePagination(searchResults);
+        displayStudents(searchResults);
     }
 }
 
 function reset() {
 
-    document.getElementById('student-search').firstElementChild.value = '';
-    document.getElementById('pagination-list').firstElementChild.click();
-    document.getElementById('pagination-list').style.display = 'block';
+    const searchError = document.getElementById('search-error'),
+        studentSearch = document.getElementById('student-search');
 
-    const searchError = document.getElementById('search-error');
+    studentSearch.firstElementChild.value = ''.trim();
+
     if (searchError) {
-        document.getElementsByClassName('page')[0].removeChild(searchError);
+        page.removeChild(searchError);
+        for (let i = 0; i < students.length; i++) {
+            students[i].style.display = 'block';
+        }
     }
+    numStudents = students.length;
+    generatePagination(students);
+    displayStudents(students);
 }
 
 createSearchBox();
-generatePagination();
-filterStudents();
+generatePagination(students);
+displayStudents(students);
 
 document.getElementById('search-button').addEventListener('click', () => {
     search();
