74b6f337eb37f7b6ab13c2d8550f604a999e21d2
Garrett Kucinski <garrettryan@doejo.com>
2017-04-21 14:39:47 -0500

    added comments for clarity

--- js/app.js
+++ js/app.js
@@ -7,8 +7,10 @@
 
 "use strict"; // no monkey business
 
+// Grab the students list and page element for global use
 const students = document.querySelectorAll('.student-item'),
     page = document.getElementsByClassName('page')[0],
+
     // Create container for the pagination element
     pagination = document.createElement('UL'),
 
@@ -17,14 +19,20 @@ const students = document.querySelectorAll('.student-item'),
     // click event accordingly
     studentsToShow = 10;
 
+// Tell the app where to start looping through students ( see displayStudents() )
 let startingStudentIndex = 0,
+    // Calculate the total number of students available
     numStudents = document.getElementsByClassName('student-item').length;
 
+// Assign the pagination list an id and class name
 pagination.id = 'pagination-list';
 pagination.className = 'pagination-list';
+
+// Append the pagination list to the page
 page.appendChild(pagination);
 
 function createSearchBox() {
+    // Function that creates and appends the search box markup
     const searchBoxMarkup = `<input placeholder="Search for students...">
         <button id="search-button">Search</button><button id="reset-button">Reset</button>`;
 
@@ -38,10 +46,13 @@ function createSearchBox() {
 }
 
 function displayStudents(students) {
+    // Hide all students initially
     for (let i = 0; i < students.length; i++) {
         students[i].style.display = 'none';
     }
 
+    // Starting with whatever student is equal to the current starting index show all students
+    // up to the amount of students to show set at the top of this file
     for (let i = startingStudentIndex; i <= (startingStudentIndex + studentsToShow); i++) {
         try {
             students[i].style.display = 'block';
@@ -90,41 +101,71 @@ function generatePagination(students) {
 }
 
 function search() {
+    // Function to govern searching the list of students and it's output of the results
 
+    // Get a reference to the pagination element and set searchResults to an empty array
+    // to contain any results of the current search
     const paginationList = document.getElementById('pagination-list'),
         searchResults = [];
 
+    // If there is a search error present set this variable equal to its contents
+    // otherwise set it to an empty string
     let searchError = document.getElementById('search-error') ? document.getElementById('search-error') : '',
+        // On search get the value of the search box, remove whitespace and set to lower case
+        // for easier and more efficient comparison
         searchTerm = document.getElementById('student-search').firstElementChild.value.trim().toLowerCase();
 
+    // Hide all students on search
     for (let i = 0; i < students.length; i++) {
         students[i].style.display = 'none';
     }
 
+    // If the user has navigated to page other than 1 we need to 
+    // reset the starting index to loop through the search results
+    // from the beginning
     startingStudentIndex = 0;
 
     if (searchTerm !== '') {
+
+        // If the user has entered anything into the search box
         for (let i = 0; i < students.length; i++) {
+
+            // loop through the list of students and pull out the name and email
             const studentName = students[i].children[0].childNodes[3].textContent.trim().toLowerCase();
             const studentEmail = students[i].children[0].childNodes[5].textContent.trim().toLowerCase();
+
             if (studentName.indexOf(searchTerm) !== -1 || studentEmail.indexOf(searchTerm) !== -1) {
+                // Push the name and email onto the searchResults array
                 searchResults.push(students[i]);
             }
+
         }
 
         if (!searchResults.length && searchError === '') {
+            // if there are no matches to the search and no errors
+
+            // hide the pagination element
             paginationList.style.display = 'none';
+
+            // and display an error message
             searchError = document.createElement('P');
             searchError.innerHTML = 'Sorry, no students found';
             searchError.className = 'search-error';
             searchError.id = 'search-error';
             page.appendChild(searchError);
         } else if (!searchResults.length && searchError !== '') {
+            // if there are no matches and an error already exists keep
+            // displaying that error
             searchError = searchError;
         } else {
+            // If there are matches 
+
             if (searchError) {
+                // and an error exists, remove it
                 page.removeChild(searchError);
             }
+
+            // then display the list of matched results
             paginationList.style.display = 'block';
             numStudents = searchResults.length;
             generatePagination(searchResults);
@@ -132,7 +173,9 @@ function search() {
         }
 
     } else {
+        // If the user executes an empty search
         if (searchError === '') {
+            // Display an error, and prevent the box from displaying multiple times
             paginationList.style.display = 'none';
             searchError = document.createElement('P');
             searchError.innerHTML = 'Please enter a student to search for.';
@@ -144,6 +187,8 @@ function search() {
 }
 
 function reset() {
+    // Allow the user to reset the application after a search or error
+
     const searchError = document.getElementById('search-error'),
         studentSearch = document.getElementById('student-search'),
         paginationList = document.getElementById('pagination-list');
@@ -172,6 +217,7 @@ function reset() {
     displayStudents(students);
 }
 
+// Initialize application with appropriate elements and event handlers
 createSearchBox();
 generatePagination(students);
 displayStudents(students);
